fc5c83c6b6ac5c6364843b9bb867f4154ffb7550
Temuge <93143245+Temugeee@users.noreply.github.com>
2023-04-22 23:50:09 +0800

    done

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className}  mx-24 my-12`}>{children}</body>
+    </html>
+  );
+}
